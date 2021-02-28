38c500cdbc291d581d99d90c4aa36972ac39a056
bonartm <malte@spiced-academy.com>
2021-02-28 16:50:18 +0100

    fix footer
    - fix visible hint on next question
    - improve styles and class names

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,45 +7,60 @@
     $: counter = quiz.counter
     $: finished = quiz.finished
     $: current =  quiz.questions[$counter]
-    $: show_hint = false
+    let show_hint = false
+    // disable hint on new question
+    $: {
+        $counter
+        show_hint = false
+    }
 
 </script>
 
-<div class="button-row">
-
-
+<div class="quizdown-button-row">
     {#if !$finished}
+        <!-- previous, disabled on first question -->
         <Button disabled={$counter === 0} buttonAction={quiz.previous}>Previous</Button>
+        <!-- help, disabled if no hint is available -->
         <span>
-            <Button disabled={current.hint === null || current.hint === ''} buttonAction={() => show_hint=!show_hint}>Help me!</Button>
+            <Button disabled={current.hint === null || current.hint === ''} buttonAction={() => show_hint = !show_hint}>Help me!</Button>
             {#if show_hint}<SpeechBubble>{@html current.hint}</SpeechBubble>{/if}
         </span>	
+        <!-- next or evaluation on last question -->
         {#if $counter === counter.max-1}
             <Button buttonAction={quiz.calc_points}>Evalution</Button>
         {:else}
             <Button buttonAction={quiz.next}>Next</Button>
-        {/if}
- 
+        {/if} 
     {:else}
+        <!-- start quiz again on result page -->
         <Button buttonAction={quiz.reset}>One more time!</Button>
     {/if}
-    </div>
+</div>
 
 <hr>
 
-<div class="credits">This quiz was created with <a href="https://github.com/bonartm/quizdown-js">quizdown-js</a> </div> 
+<div class="quizdown-credits">This quiz was created with <a href="https://github.com/bonartm/quizdown-js">quizdown-js</a>.</div> 
 
 
 <style>
-    .button-row {
+    .quizdown-button-row {
         margin-top:0.8em;
         justify-content: center;
         display: inline-flex;
     }
-    
-    .credits {
+
+    .quizdown-credits a {
+        color: darkgray;
+    }
+
+    .quizdown-credits a:hover {
+        text-decoration: underline;
+    }
+
+    .quizdown-credits {
 		font-size: small;
 		text-align:end;
+        color:gray;
 	}
 
     hr {
