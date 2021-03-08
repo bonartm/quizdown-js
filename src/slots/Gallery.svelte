79597d4c6d7410356890dcfe97a9328c451c7773
bonartm <malte@spiced-academy.com>
2021-03-08 15:16:40 +0100

    experimental gallery component

---
+++ src/slots/Gallery.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { fly, fade } from "svelte/transition";
+
+    let direction = -1;
+    export let key;      
+</script>
+
+<div class='gallery-wrapper'>
+    {#key key}
+        <div class='gallery-item'
+            in:fade={{duration: 300, delay: 300 }}
+            out:fade={{duration: 300 }}>
+            <slot></slot>            
+        </div>
+    {/key}  
+</div>
+
+
+
+<style>
+
+    .gallery-wrapper {
+        display: grid;
+        align-items: start;
+    }
+
+    .gallery-item {
+        grid-row-start:1;
+        grid-column-start:1;
+    }
+</style>
+
+
