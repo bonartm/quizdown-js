6131a7892224a6e3e1d84b2bc5d80c93cfc11718
bonartm <malte@spiced-academy.com>
2021-03-08 15:10:32 +0100

    restructure main components

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,33 @@
 <script lang="ts">
-import ResultSection from './components/ResultSection.svelte';
 import QuizSection from './components/QuizSection.svelte';
 import Footer from './components/Footer.svelte';
 import type { Quiz } from './quiz';
 import ProgressBar from './components/ProgressBar.svelte'
-import 'picnic/picnic.css'
 
 export let quiz: Quiz;
-$: finished = quiz.finished
+
+
 </script>
 
 <div class='quizdown-content'>
-		<ProgressBar quiz={quiz}/>
-
-{#if !$finished}
+	<ProgressBar quiz={quiz}/>
 	<QuizSection quiz={quiz}></QuizSection>
-{:else}
-	<ResultSection quiz={quiz}></ResultSection>
-{/if}
-
-<Footer quiz={quiz}></Footer>
-
-	
-
+	<Footer quiz={quiz}></Footer>
 </div>
 
 <style>
-   :global(.quizdown-content) {	
-		font-family: 'Lucida Console', 'Courier New', monospace;
+   :global(html body .quizdown-content) {
+	   	margin:auto;
+	    max-width: 600px;
 		width:100%;  	 
 		display:flex;
 		flex-direction: column;
 		justify-content: center; 
-    }
-
+		--quizdown-color-primary:  #FF851B  ;
+		--quizdown-color-secondary: #DDDDDD;
+		font-family: 'Lucida Console', 'Courier New', monospace;
+		padding:1em;
+		margin-top:1em;
+		margin-bottom:1em;
+	}
 </style>
--- src/components/QuizSection.svelte
+++ src/components/QuizSection.svelte
@@ -1,33 +1,42 @@
 <script lang="ts">
     import type { Quiz } from '../quiz';
+
     import SequenceView from './SequenceView.svelte';
     import MultipleChoiceView from './MultipleChoiceView.svelte'
     import BlanksView from './BlanksView.svelte'
     import PairsView from './PairsView.svelte'
     import Headline from '../slots/Headline.svelte'
-    
+    import ResultView from './ResultView.svelte'
+    import Gallery from '../slots/Gallery.svelte'
+
     export let quiz: Quiz;
     $: counter = quiz.counter
     $: current = quiz.questions[$counter]
-    
+    $: finished = quiz.finished
+
     const views = {
         'MultipleChoice': MultipleChoiceView,
         'Sequence': SequenceView,
         'Gaps': BlanksView,
         'Pairs': PairsView
     }
-
-    
+  
 </script>
 
-<Headline>
-    <h3>{@html current.text}</h3>
-</Headline>
+<!-- <Gallery key={$counter}> -->
+    {#if $finished}
+        <!-- Results -->
+        <Headline>Your quiz results</Headline>
+        <ResultView quiz={quiz}/>    
+    {:else}
+        <!-- Question  -->
+        <Headline>{@html current.text}</Headline>
+        {#if current.explanation !== null && current.explanation !== ''}
+            <p class='quizdown-explanation'>{@html current.explanation}</p>
+        {/if}
+        <svelte:component this={views[current.type]} quiz={quiz}/>    
+    {/if}
+<!-- </Gallery>   -->
 
-<p>
-    {@html current.explanation}
-</p>
-
-<svelte:component this={views[current.type]} quiz={quiz}/>
 
 
