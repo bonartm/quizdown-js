8cc4fc4561e32e74ceb09e92758e23aaec3e4351
bonartm <malte@spiced-academy.com>
2021-05-10 15:35:35 +0200

    fixes #6

--- src/components/SequenceView.svelte
+++ src/components/SequenceView.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import DragDropList from 'svelte-dragdroplist';
+    import DragDropList from './DragDropList.svelte';
     import type { Quiz } from '../quiz';
     export let quiz: Quiz;
     $: counter = quiz.counter;
@@ -11,25 +11,3 @@
 </script>
 
 <DragDropList bind:data="{current.answers}" />
-
-<style>
-    :global(.dragdroplist > .list > div.item) {
-        min-height: 2rem;
-        height: 2.5rem;
-        margin: 5px;
-        padding: 0;
-        width: unset;
-        background-color: var(--quizdown-color-secondary);
-        border: 3px solid transparent;
-    }
-
-    :global(.dragdroplist div.content) {
-        padding: 0;
-        margin: auto auto auto 0 !important;
-    }
-
-    :global(.dragdroplist #ghost) {
-        border: 3px solid var(--quizdown-color-primary);
-        background-color: var(--quizdown-color-secondary);
-    }
-</style>
