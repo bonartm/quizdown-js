463b08a39d3ba9d084c697da5f81b32cbc8ad1df
bonartm <malte@spiced-academy.com>
2021-07-02 20:15:46 +0200

    fix and simplify progress bar
    - remove quiz from progress bar
    - prevent to exceed app boundaries

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -1,41 +1,32 @@
 <script lang="ts">
     import { tweened } from 'svelte/motion';
     import { cubicOut } from 'svelte/easing';
-    import type { Quiz } from '../quiz';
 
-    export let quiz: Quiz;
-    $: counter = quiz.counter;
-    $: finished = quiz.finished;
+    export let value: number;
+    export let max: number;
 
-    const animatedCurrentBlock = tweened(0, {
+    const progress = tweened(0, {
         duration: 400,
         easing: cubicOut,
     });
-
-    $: {
-        if ($finished) {
-            animatedCurrentBlock.set(quiz.counter.max - 0.5);
-        } else {
-            animatedCurrentBlock.set($counter + 0.1);
-        }
-    }
-
-    $: progressPercent =
-        String(($animatedCurrentBlock / (quiz.counter.max - 0.5)) * 100) + '%';
+    $: progress.set(Math.min(max, value) + 0.5);
+    $: progressPercent = String(($progress / (max + 0.5)) * 100) + '%';
 </script>
 
-<div class="quizdown-progress" data-label="">
+<div class="progress" data-label="">
     <div class="progress-slider" style="width:{progressPercent}"></div>
 </div>
 
 <style>
-    .quizdown-progress {
+    .progress {
+        grid-area: auto;
+
         height: 0.4em;
         width: 100%;
         position: relative;
     }
 
-    .quizdown-progress .progress-slider {
+    .progress .progress-slider {
         background-color: var(--quizdown-color-primary);
         height: 100%;
         display: block;
