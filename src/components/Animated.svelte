46be97c5f9ee245e080242cf2e0535dffb850a09
bonartm <malte@spiced-academy.com>
2021-07-06 18:29:37 +0200

    fadeIn for question elements

---
+++ src/components/Animated.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { fade } from 'svelte/transition';
+    export let update: any;
+
+    // function fixPosition(node, { delay = 0, duration = 400, width = 300 }) {
+    //     return {
+    //         delay,
+    //         duration,
+    //         css: function (t) {
+    //             return `height: 0px; overflow:hidden;`;
+    //         },
+    //     };
+    // }
+</script>
+
+{#key update}
+    <div class="animated" in:fade="{{ duration: 600 }}">
+        <slot />
+    </div>
+{/key}
+
+<style>
+</style>
