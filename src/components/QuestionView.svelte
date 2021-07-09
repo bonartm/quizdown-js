b6d832d1b5dbf05a8101e9c15042b1e9aee95f17
bonartm <malte@spiced-academy.com>
2021-07-09 20:17:12 +0200

    put hint button to question header

--- src/components/QuestionView.svelte
+++ src/components/QuestionView.svelte
@@ -2,12 +2,16 @@
     import type { QuizType, BaseQuestion } from '../quiz';
     import type { SvelteComponent } from 'svelte';
 
+    import Button from './Button.svelte';
+    import Icon from './Icon.svelte';
+
     import Hint from './Hint.svelte';
     import SequenceView from './SequenceView.svelte';
     import ChoiceView from './ChoiceView.svelte';
     import { _ } from 'svelte-i18n';
 
     export let question: BaseQuestion;
+
     export let n: number;
     $: showHint = question.showHint;
 
@@ -19,10 +23,20 @@
     };
 </script>
 
-<h3>Q{n}: {@html question.text}</h3>
+<h3>
+    Q{n}: {@html question.text}
+
+    <Button
+        title="{$_('hint')}"
+        disabled="{!question.hint || $showHint}"
+        buttonAction="{question.enableHint}"><Icon name="lightbulb" /></Button
+    >
+</h3>
+
 {#if question.explanation}
     <p>{@html question.explanation}</p>
 {/if}
+
 <Hint hint="{question.hint}" show="{$showHint}" />
 <svelte:component
     this="{componentMap[question.quizType]}"
