4f13bbe2d31bdb4b4467418bdbb9ee364a1fccf8
bonartm <malte@spiced-academy.com>
2021-07-12 18:14:20 +0200

    move hint to bottom

--- src/components/QuestionView.svelte
+++ src/components/QuestionView.svelte
@@ -5,7 +5,6 @@
     import Button from './Button.svelte';
     import Icon from './Icon.svelte';
 
-    import Hint from './Hint.svelte';
     import SequenceView from './SequenceView.svelte';
     import ChoiceView from './ChoiceView.svelte';
     import { _ } from 'svelte-i18n';
@@ -25,19 +24,12 @@
 
 <h3>
     Q{n}: {@html question.text}
-
-    <Button
-        title="{$_('hint')}"
-        disabled="{!question.hint || $showHint}"
-        buttonAction="{question.enableHint}"><Icon name="lightbulb" /></Button
-    >
 </h3>
 
 {#if question.explanation}
     <p>{@html question.explanation}</p>
 {/if}
 
-<Hint hint="{question.hint}" show="{$showHint}" />
 <svelte:component
     this="{componentMap[question.quizType]}"
     question="{question}"
