2dcc7b9219a3eceac334080832cd6c0b97418500
bonartm <malte@spiced-academy.com>
2021-07-12 18:13:58 +0200

    replace buttons with row component

---
+++ src/components/Row.svelte
@@ -0,0 +1,35 @@
+<div class="row">
+    <div class="left">
+        <slot name="left" />
+    </div>
+    <div class="center">
+        <slot class="center" name="center" />
+    </div>
+    <div class="right">
+        <slot name="right" />
+    </div>
+</div>
+
+<style>
+    .row {
+        padding-top: 2em;
+        display: flex;
+    }
+
+    .left {
+        flex: 1;
+        display: flex;
+        justify-content: flex-start;
+    }
+
+    .center {
+        display: flex;
+        justify-content: center;
+    }
+
+    .right {
+        flex: 1;
+        display: flex;
+        justify-content: flex-end;
+    }
+</style>
